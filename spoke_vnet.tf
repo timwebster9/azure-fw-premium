62c2075d08b4297a3ae208971995ed27b3815101
Tim Webster <tim.webster@contino.io>
2021-07-07 14:05:46 +0100

    route spoke appgw traffic back to firewall

--- spoke_vnet.tf
+++ spoke_vnet.tf
@@ -22,6 +22,8 @@ resource "azurerm_subnet" "spoke-appgw" {
 }
 
 # ROUTES
+
+# default route for spoke VM subnet
 resource "azurerm_route_table" "spoke_subnet" {
   name                          = "spoke-rt"
   location                      = azurerm_resource_group.poc-vnet.location
@@ -34,10 +36,18 @@ resource "azurerm_route_table" "spoke_subnet" {
     next_hop_type  = "VirtualAppliance"
     next_hop_in_ip_address = "10.124.4.4"
   }
+}
+
+# route traffic from spoke app gateway to backends back to firewall for IDPS
+resource "azurerm_route_table" "spoke_appgw_subnet" {
+  name                          = "spoke-rt"
+  location                      = azurerm_resource_group.poc-vnet.location
+  resource_group_name           = azurerm_resource_group.poc-vnet.name
+  disable_bgp_route_propagation = false
 
   route {
-    name           = "appgw-to-firewall"
-    address_prefix = var.cidr_hub_subnet_appgw
+    name           = "default-to-firewall"
+    address_prefix = var.cidr_spoke_subnet_default
     next_hop_type  = "VirtualAppliance"
     next_hop_in_ip_address = "10.124.4.4"
   }
@@ -46,4 +56,9 @@ resource "azurerm_route_table" "spoke_subnet" {
 resource "azurerm_subnet_route_table_association" "spoke-rt-association" {
   subnet_id      = azurerm_subnet.spoke.id
   route_table_id = azurerm_route_table.spoke_subnet.id
+}
+
+resource "azurerm_subnet_route_table_association" "spokeappgw-rt-association" {
+  subnet_id      = azurerm_subnet.spoke.id
+  route_table_id = azurerm_route_table.spoke_appgw_subnet.id
 }
